ce562aa2203baa6c9da1a5de3f21b3363c26c99f
Vivek Singh <vivekkrvks@gmail.com>
2023-09-02 01:41:25 +0530

    fix

--- Frontend/app/layout.js
+++ Frontend/app/layout.js
@@ -2,7 +2,8 @@
 
 import React from 'react';
 import LayoutWrapper from './LayoutWrapper'; // Import the wrapper component
- 
+import { metadata } from './globals.css';
+
 export const metadata = {
   title: 'Oasis Manors, Inc - Assisted Living Facility at 15116 Roxford St, Sylmar, CA 91342',
   description: 'Best Assisted Living Facility for Elderly. Get Nutritious homestyle meals and furnished rooms. Emergency call system and 24-hour supervision. Low-impact wellness program in pool and spa. Games, walks, local visits, shopping and regular yoga activities. Grab all facilities at one place - Oasis Manors. Call 310-995-4859',
